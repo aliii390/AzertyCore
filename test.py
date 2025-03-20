49d31006aa2b231800b5cf56c9ff97418ecefeb6
aliii390 <alli.said.errahmani@gmail.com>
2025-03-20 17:00:28 +0100

    fin

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
+from langchain_mistralai.chat_models import ChatMistralAI
+from langchain_mistralai.embeddings import MistralAIEmbeddings
+# from langchain_community.chat_models import ChatMistralAI
+import dotenv
+# import getpass
+import os
 
-text = "./data/books/AliceInWonderland.txt"
-from langchain import RecursiveCharacterTextSplitter
+dotenv.load_dotenv()
+# if "MISTRAL_API_KEY" not in os.environ:
+#     os.environ["MISTRAL_API_KEY"] = getpass.getpass("Enter your Mistral API key: ")
 
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=500,
-    length_function=len,
-    add_start_index=True,
-)
 
-chunks = text_splitter.split_text(text) 
+llm = ChatMistralAI()
+
+
+print(llm.invoke(input()).content)
